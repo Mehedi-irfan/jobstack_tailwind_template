093c62dcc4f748d1087d8efc1699522cde6b2ede
Mehedi-irfan <mehediirfan99@gmal.com>
2023-04-01 16:45:39 +0600

    initilize the tailwind project jobstack

---
+++ Js/Script.js
@@ -0,0 +1,6 @@
+let navMenu = document.getElementById("nav-menu");
+let navBar = document.getElementById("menu-bar");
+
+navBar.addEventListener("click", () => {
+  navMenu.classList.toggle("hidden");
+});
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      serif: ["Roboto Slab", "serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
