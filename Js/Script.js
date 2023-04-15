24bd809c37b4ac9bf166f80d58ab811c6d82a1de
Mehedi-irfan <mehediirfan99@gmal.com>
2023-04-15 23:20:26 +0600

    dark mode and light mode added

--- Js/Script.js
+++ Js/Script.js
@@ -5,4 +5,18 @@ navBar.addEventListener("click", () => {
   navMenu.classList.toggle("hidden");
 });
 
-// Swiper js Slider
+// scroll btn
+const header = document.getElementById("headerId");
+const scrollBtn = document.getElementById("scollBtn");
+
+scrollBtn.addEventListener("click", () => {
+  header.scrollIntoView({ behavior: "smooth" });
+});
+
+// dark mode light mode
+const switchToggle = document.getElementById("switch-toggle");
+const mainHtml = document.getElementById("mainHtml");
+
+switchToggle.addEventListener("click", () => {
+  mainHtml.classList.toggle("dark");
+});
